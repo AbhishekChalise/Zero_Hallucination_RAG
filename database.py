e257708651a223047080b8c2a17f50706d712b3e
AbhishekChalise <abhishekchalise18@gmail.com>
2026-09-10 16:10:24 +0545

    feat: implement hybrid_data_dictionary function to process and embed dataset

--- database.py
+++ database.py
@@ -9,4 +9,33 @@ def load_dataset():
     return my_list
 
 
+def hybrid_data_dictonary():
+
+    data = load_dataset()
+
+    hybrid_data = []
+    batch_size = 64
+
+    for item in range(0, len(data), batch_size):
+        data_i = data[item: item + batch_size]
+
+        text = [chunk["text"] for chunk in data_i]
+
+        vectors = llm.embedder_model(text)
+
+        for chunk, vector in zip(data_i, vectors):
+            hybrid_data.append(
+                {
+                  "vector": vector,
+                  "text": data_i["text"],
+                  "title": data_i['title'],
+                  "summary": data_i['summary']
+                }
+            )
+
+    return hybrid_data
+
+
+
+
 
